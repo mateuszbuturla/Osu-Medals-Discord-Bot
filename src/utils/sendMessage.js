f5e992abf9a3ba5f1c1d5d1a6bcaaf2158e32c3f
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-08-16 23:13:02 +0200

    add optional sendMessage attribute to add reaction to messageg

--- src/utils/sendMessage.js
+++ src/utils/sendMessage.js
@@ -1,10 +1,16 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = (client, title, status, description, fields) => {
+module.exports = (client, title, status, description, fields, reactions) => {
     const embed = new MessageEmbed()
         .setTitle(title)
         .setColor(status === 'correct' ? 0x32a852 : 0xdb1a1a)
         .setDescription(description)
         .addFields(fields);
-    client.channel.send(embed);
+    client.channel.send(embed).then((message) => {
+        if (reactions !== undefined && reactions.length > 0) {
+            reactions.map((reaction) => {
+                message.react(reaction);
+            });
+        }
+    });
 };
