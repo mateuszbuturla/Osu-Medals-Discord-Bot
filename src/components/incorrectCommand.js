a65472eabfe8edf94571cfd676db462b8f27e972
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-08-16 22:52:03 +0200

    update incorrect command message

--- src/components/incorrectCommand.js
+++ src/components/incorrectCommand.js
@@ -5,6 +5,6 @@ module.exports = (message) => {
         message,
         'Incorrect command',
         'error',
-        'This command does not exist',
+        `This command does not exist. Type ${prefix}help to get all commands list.`,
     );
 };
