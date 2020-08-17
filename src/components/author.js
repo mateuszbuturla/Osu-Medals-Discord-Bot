1995897757f2fe8fb0e39d8be312cda4b88882dd
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-08-17 21:17:23 +0200

    edit author command content

--- src/components/author.js
+++ src/components/author.js
@@ -1,8 +1,14 @@
 const sendMessage = require('../utils/sendMessage');
 
 module.exports = (message) => {
-    sendMessage(message, 'Osu medals bot author', 'correct', 'Author', null, {
-        name: 'Discord',
-        value: 'Bucik689#5588',
-    });
+    sendMessage(message, 'Osu medals bot author', 'correct', 'Author', null, [
+        {
+            name: 'Discord',
+            value: 'Bucik689#5588',
+        },
+        {
+            name: 'Email',
+            value: 'bucik689@gmail.com',
+        },
+    ]);
 };
