635825478ede5e10b75583a23a7a5dab09dd6f4a
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-08-17 12:21:58 +0200

    add forward and backward buttons to allMedals

--- src/components/allMedals.js
+++ src/components/allMedals.js
@@ -1,16 +1,67 @@
 const sendMessage = require('../utils/sendMessage');
 
 module.exports = (message) => {
+    const allMedals = data.map((medal) => {
+        return {
+            name: medal.title,
+            value: medal.description,
+        };
+    });
+
+    let pages = [
+        allMedals.slice(0, 5),
+        allMedals.slice(5, 10),
+        allMedals.slice(10, 15),
+        allMedals.slice(15, 18),
+    ];
+    let page = 1;
+
+    const thenFunction = (msg, embed) => {
+        msg.react('⏪').then((r) => {
+            msg.react('⏩');
+
+            const backwardsFilter = (reaction, user) =>
+                reaction.emoji.name === '⏪' && user.id === message.author.id;
+            const forwardsFilter = (reaction, user) =>
+                reaction.emoji.name === '⏩' && user.id === message.author.id;
+
+            const backwards = msg.createReactionCollector(backwardsFilter, {
+                time: 60000,
+            });
+            const forwards = msg.createReactionCollector(forwardsFilter, {
+                time: 60000,
+            });
+
+            backwards.on('collect', (r, u) => {
+                r.users.remove(
+                    r.users.cache.filter((u) => u === message.author).first(),
+                );
+                if (page === 1) return;
+                page--;
+                embed.fields = pages[page - 1];
+                embed.setFooter(`Page ${page} of ${pages.length}`);
+                msg.edit(embed);
+            });
+
+            forwards.on('collect', (r, u) => {
+                r.users.remove(
+                    r.users.cache.filter((u) => u === message.author).first(),
+                );
+                if (page === pages.length) return;
+                page++;
+                embed.fields = pages[page - 1];
+                embed.setFooter(`Page ${page} of ${pages.length}`);
+                msg.edit(embed);
+            });
+        });
+    };
+
     sendMessage(
         message,
         'All medals',
         'correct',
         'All medals in Osu standard mode',
-        data.map((medal) => {
-            return {
-                name: medal.title,
-                value: medal.description,
-            };
-        }),
+        pages[page - 1],
+        thenFunction,
     );
 };
--- src/utils/sendMessage.js
+++ src/utils/sendMessage.js
@@ -1,16 +1,14 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = (client, title, status, description, fields, reactions) => {
+module.exports = (client, title, status, description, fields, thenFunction) => {
     const embed = new MessageEmbed()
         .setTitle(title)
         .setColor(status === 'correct' ? 0x32a852 : 0xdb1a1a)
         .setDescription(description)
         .addFields(fields);
-    client.channel.send(embed).then((message) => {
-        if (reactions !== undefined && reactions.length > 0) {
-            reactions.map((reaction) => {
-                message.react(reaction);
-            });
+    client.channel.send(embed).then((msg) => {
+        if (thenFunction !== undefined) {
+            thenFunction(msg, embed);
         }
     });
 };
