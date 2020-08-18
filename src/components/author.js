57083c96ed8d5b1ed6a33e9ed052b94686ceed64
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-08-18 12:11:55 +0200

    add author icon

--- src/components/author.js
+++ src/components/author.js
@@ -1,14 +1,23 @@
 const sendMessage = require('../utils/sendMessage');
 
 module.exports = (message) => {
-    sendMessage(message, 'Osu medals bot author', 'correct', 'Author', null, [
-        {
-            name: 'Discord',
-            value: 'Bucik689#5588',
-        },
-        {
-            name: 'Email',
-            value: 'bucik689@gmail.com',
-        },
-    ]);
+    sendMessage(
+        message,
+        'Osu medals bot author',
+        'correct',
+        'Author',
+        null,
+        [
+            {
+                name: 'Discord',
+                value: 'Bucik689#5588',
+            },
+            {
+                name: 'Email',
+                value: 'bucik689@gmail.com',
+            },
+        ],
+        null,
+        'https://cdn.discordapp.com/avatars/355348267133042689/9a5f6e229cd3f342605150edb1920eb9.png?size=128',
+    );
 };
